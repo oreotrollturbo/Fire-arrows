13ac542ea3584b116b4b728632b3f007386f4e50
The60th <justinthe60th@gmail.com>
2025-01-03 22:42:08 -0500

    Changes

--- src/main/kotlin/org/oreo/fireArrows/listeners/BowLightEvent.kt
+++ src/main/kotlin/org/oreo/fireArrows/listeners/BowLightEvent.kt
@@ -1,5 +1,7 @@
 package org.oreo.fireArrows.listeners
 
+import net.kyori.adventure.text.Component
+import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.entity.Player
@@ -17,9 +19,27 @@ class BowLightEvent(private val plugin : FireArrows) : Listener {
 
         val player = e.player //Get the player
         val block: Block? = e.clickedBlock //Get the block
-
+        System.out.println("bow light event1")
         if (!isHoldingBow(player) || !isCorrectBlock(block)) return
+        System.out.println("bow light event2")
+        //Remove an item from their inventory
 
+        //We want to remove one coal block from the player inventory as a cost to light
+        //the arrow on fire.
+
+        val inventory = player.inventory
+
+        if (inventory.contains(Material.COAL_BLOCK)) {
+            val slot = inventory.first(Material.COAL_BLOCK)
+            val itemStack = inventory.getItem(slot)
+            itemStack!!.amount = itemStack.amount - 1
+            inventory.setItem(slot,itemStack)
+            System.out.println("bow light event3")
+        }else{
+            player.sendActionBar(Component.text(ChatColor.RED.toString() + "You need a coal block to set ur arrow aflame!"))
+            return
+        }
+        System.out.println("bow light event4")
         FireArrowUtils.givePlayerFireArrow(player,block!!,plugin)
     }
 
@@ -28,7 +48,9 @@ class BowLightEvent(private val plugin : FireArrows) : Listener {
      * Makes sure the player is holding a bow
      */
     private fun isHoldingBow(player: Player): Boolean { //What item the player needs to be holding a bow
+        System.out.println("bow light event5 " + player.inventory.itemInMainHand.type)
         return player.inventory.itemInMainHand.type == Material.BOW || player.inventory.itemInMainHand.type == Material.CROSSBOW
+
     }
 
     /**
@@ -36,6 +58,6 @@ class BowLightEvent(private val plugin : FireArrows) : Listener {
      */
     private fun isCorrectBlock(block: Block?): Boolean { //Player needs to be interacting with a magma block fire or a campfire
         if (block == null) return false
-        return block.blockData.material == Material.FIRE || block.blockData.material == Material.SOUL_CAMPFIRE
+        return block.blockData.material == Material.CAMPFIRE
     }
 }
